c5f6352944ce30047b6c99cb4d781f07d12e33c6
Shane Lillie <ignignokterr@gmail.com>
2022-01-11 14:28:27 -0800

    Build packages for musl target

--- build-package.sh
+++ build-package.sh
@@ -4,13 +4,14 @@ set -e
 
 VERSION=`git rev-parse --short HEAD`
 
+rm -rf build/
 rm -f echo*.zip
 
 mkdir build
 
 cp install.sh build
 
-cargo install --path . --root build/
+cargo install --target x86_64-unknown-linux-musl --path . --root build/
 rm build/.crates*
 
 cd build
