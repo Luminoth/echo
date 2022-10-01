4cdfe5c8faa994501d6441b24cb3668379309d8d
Shane Lillie <ignignokterr@gmail.com>
2022-09-30 19:26:56 -0700

    More re-organizing

--- bin/build-package.sh
+++ bin/build-package.sh
@@ -5,12 +5,14 @@ set -e
 VERSION=`git rev-parse --short HEAD`
 PACKAGE_NAME=echo-$VERSION.zip
 
+cd ..
 rm -rf build/
-rm -f echo*.zip
+rm -f  echo*.zip
 
 mkdir build
 
-cp install.sh build
+cp echo/install.sh build/
+chmod 755 echo/install.sh
 
 cd echo
 cargo install --target x86_64-unknown-linux-musl --path . --root ../build/
